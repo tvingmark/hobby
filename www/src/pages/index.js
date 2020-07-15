e894d59e9d1a722b75607a485b290006d29a8f23
tvingmark <emmigunn+os@gmail.com>
2020-07-15 18:13:46 +0000

    data credit

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -15,7 +15,8 @@ const IndexPage = () => (
             }}
             src="http://www.vegagerdin.is/vgdata/vefmyndavelar/myndir/now/landvegamot_4.jpg" />
         <img src="https://www.vedur.is/photos/thattaspa_igb_su_urk-msl-10uv/200715_1200_006.gif"/>
-        <p>ÉG ELSKA ÞIG EDDA</p>  
+        <p>ÉG ELSKA ÞIG EDDA</p>
+        <p>Byggt á gögnum frá Vegagerðinni & Veðurstofu Íslands</p>
     </div>  
 )
 
