86b03725bba46091f7b9cc3a038d5c28dbaeaea8
tvingmark <emmigunn+os@gmail.com>
2020-07-15 18:03:13 +0000

    remove margin

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -9,7 +9,12 @@ const IndexPage = () => (
     <div>
         Dellan.is
         <h1 id="subdomain"></h1>
-        <img src="http://www.vegagerdin.is/vgdata/vefmyndavelar/myndir/now/landvegamot_4.jpg" />
+        <img
+            style={{
+                margin: '0'
+            }}
+            src="http://www.vegagerdin.is/vgdata/vefmyndavelar/myndir/now/landvegamot_4.jpg" />
+        <img src="https://www.vedur.is/photos/thattaspa_igb_su_urk-msl-10uv/200715_1200_006.gif"/>
         <p>ÉG ELSKA ÞIG EDDA</p>  
     </div>  
 )
