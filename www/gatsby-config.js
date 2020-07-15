185fa141fac228cb491dec3f72681261ad5ce9f9
tvingmark <emmigunn+os@gmail.com>
2020-07-15 17:47:39 +0000

    added gatsby s3 upload + invalidatin

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -1,10 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Dellan is..`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+        resolve: `gatsby-plugin-s3`,
+        options: {
+            bucketName: `empty-dellan`,
+        }
+    },   
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
